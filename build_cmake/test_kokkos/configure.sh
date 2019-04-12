8a3734a1677051059c5221da8ad3c09d65f7bb7d
Daniel Arndt <arndtd@ornl.gov>
2019-04-12 18:15:21 +0000

    Improve CMake build

--- build_cmake/test_kokkos/configure.sh
+++ build_cmake/test_kokkos/configure.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-KOKKOS_DIR=/tmp/kokkos/build_gen/build/install
 mkdir -p build && cd build || exit 1
-cmake -DCMAKE_CXX_COMPILER=${KOKKOS_DIR}/bin/nvcc_wrapper -DKOKKOS_DIR=${KOKKOS_DIR} ..
-make -j
+cmake -DCMAKE_CXX_COMPILER=${KOKKOS_DIR}/bin/nvcc_wrapper ..
+make VERBOSE=1
