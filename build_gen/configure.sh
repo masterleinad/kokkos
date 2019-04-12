6bbcbcb8c0e36e2af6026e62e5ab0ba9f985d5a6
Daniel Arndt <arndtd@ornl.gov>
2019-04-12 14:57:12 +0000

    fix gen

--- build_gen/configure.sh
+++ build_gen/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 KOKKOS_PATH=/tmp/kokkos
 
-mkdir build && cd build || exit 1
+mkdir -p build && cd build || exit 1
 ${KOKKOS_PATH}/generate_makefile.bash --arch=Volta70 --with-cuda
 
 make -j
