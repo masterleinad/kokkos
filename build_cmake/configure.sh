dfd46cbc5fa0d0991e732162aa78fb4b2d38bdaa
Daniel Arndt <arndtd@ornl.gov>
2019-04-12 16:05:30 +0000

    cmake/pkg working

--- build_cmake/configure.sh
+++ build_cmake/configure.sh
@@ -4,8 +4,14 @@ export KOKKOS_PATH=/tmp/kokkos
 mkdir -p build && cd build || exit 1
 cmake -DKOKKOS_ARCH=Volta70 \
       -DKOKKOS_ENABLE_CUDA=ON \
+      -DKOKKOS_ENABLE_CUDA_LAMBDA=ON \
       -DCMAKE_CXX_COMPILER=/tmp/kokkos/bin/nvcc_wrapper \
       -DCMAKE_INSTALL_PREFIX=${PWD}/install \
       ${KOKKOS_PATH}
 make -j
 make -j install
+
+#need nvcc_wrapper to compile
+
+export PKG_CONFIG_PATH=${PWD}/install/lib/pkgconfig/:${PKG_CONFIG_PATH}
+echo "PKG_CONFIG_PATH: ${PKG_CONFIG_PATH}"
