caf3abd7a98c351be3fa788bf0d6ba06e5c10b17
Daniel Arndt <arndtd@ornl.gov>
2019-04-15 17:54:49 +0000

    cmake/clang/pkg working

---
+++ build_cmake_clang/configure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export KOKKOS_PATH=/tmp/kokkos
+
+mkdir -p build && cd build || exit 1
+cmake -DKOKKOS_ARCH=SNB,Volta70 \
+      -DKOKKOS_ENABLE_CUDA=ON \
+      -DKOKKOS_ENABLE_OPENMP=ON \
+      -DKOKKOS_ENABLE_CUDA_LAMBDA=ON \
+      -DCMAKE_CXX_COMPILER=clang++-7 \
+      -DCMAKE_INSTALL_PREFIX=${PWD}/install \
+      ${KOKKOS_PATH}
+make -j
+make -j install
+
+#need nvcc_wrapper to compile
+
+export PKG_CONFIG_PATH=${PWD}/install/lib/pkgconfig/:${PKG_CONFIG_PATH}
+echo "PKG_CONFIG_PATH: ${PKG_CONFIG_PATH}"
+
+tree ${KOKKOS_PATH}/build_cmake/build/install &> ${KOKKOS_PATH}/build_cmake/cmake_tree
