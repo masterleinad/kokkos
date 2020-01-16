20d425ed1366947e6633c4d5bc9b75265b4828ca
Dan Ibanez <daibane@sandia.gov>
2020-01-16 13:39:35 -0700

    Some more Windows fixes

--- core/src/impl/Kokkos_Core.cpp
+++ core/src/impl/Kokkos_Core.cpp
@@ -50,7 +50,9 @@
 #include <cstdlib>
 #include <stack>
 #include <cerrno>
+#ifndef _WIN32
 #include <unistd.h>
+#endif
 
 //----------------------------------------------------------------------------
 
@@ -462,6 +464,16 @@ bool check_int_arg(char const* arg, char const* expected, int* value) {
 
 namespace Kokkos {
 
+namespace Impl {
+unsigned get_process_id() {
+#ifdef _WIN32
+  return unsigned(GetCurrentProcessId());
+#else
+  return unsigned(getpid());
+#endif
+}
+}
+
 void initialize(int& narg, char* arg[]) {
   int num_threads       = -1;
   int numa              = -1;
@@ -782,7 +794,7 @@ void initialize(int& narg, char* arg[]) {
             "Error: cannot KOKKOS_SKIP_DEVICE the only KOKKOS_RAND_DEVICE. "
             "Raised by Kokkos::initialize(int narg, char* argc[]).");
 
-      std::srand(getpid());
+      std::srand(Impl::get_process_id());
       while (device < 0) {
         int test_device = std::rand() % rdevices;
         if (test_device != skip_device) device = test_device;
