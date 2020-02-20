0e3fff219ee52a1f668bf5f4c67d61abcb2b0028
Daniel Arndt <arndtd@ornl.gov>
2020-02-20 12:26:24 -0500

    Don't use 'not'

--- core/src/impl/Kokkos_Atomic_Windows.hpp
+++ core/src/impl/Kokkos_Atomic_Windows.hpp
@@ -73,17 +73,16 @@ __attribute__((aligned(16)))
 ;
 }  // namespace Impl
 
-KOKKOS_INLINE_FUNCTION char atomic_compare_exchange(
-    volatile char* const dest, const char& compare,
-    const char& val) {
+KOKKOS_INLINE_FUNCTION char atomic_compare_exchange(volatile char* const dest,
+                                                    const char& compare,
+                                                    const char& val) {
   char result = _InterlockedCompareExchange8(dest, val, compare);
   return result;
 }
 
-
 KOKKOS_INLINE_FUNCTION short atomic_compare_exchange(volatile short* const dest,
-                                                    const short& compare,
-                                                    const short& val) {
+                                                     const short& compare,
+                                                     const short& val) {
   short result = _InterlockedCompareExchange16(dest, val, compare);
   return result;
 }
