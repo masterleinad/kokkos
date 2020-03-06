30356fefa1946b49861c5676048492920990d1a9
Daniel Arndt <arndtd@ornl.gov>
2020-03-06 17:49:40 -0500

    Down to one(5)

--- core/unit_test/TestMDRange_b.hpp
+++ core/unit_test/TestMDRange_b.hpp
@@ -46,6 +46,8 @@
 
 namespace Test {
 
+// WORKAROUND MSVC
+#ifndef _WIN32
 TEST(TEST_CATEGORY, mdrange_6d) {
   TestMDRange_6D<TEST_EXECSPACE>::test_for6(10, 10, 10, 10, 5, 5);
 #if !defined(KOKKOS_ENABLE_ROCM)  // MDRange Reduce explicitly handled in its
@@ -53,5 +55,6 @@ TEST(TEST_CATEGORY, mdrange_6d) {
   TestMDRange_6D<TEST_EXECSPACE>::test_reduce6(100, 10, 10, 10, 5, 5);
 #endif
 }
+#endif
 
 }  // namespace Test
