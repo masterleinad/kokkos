a9cc67e7edfbe12393835c642e0302555c330612
Daniel Arndt <arndtd@ornl.gov>
2020-03-19 12:17:10 -0500

    complexdouble failing

--- core/unit_test/TestAtomicOperations_longint.hpp
+++ core/unit_test/TestAtomicOperations_longint.hpp
@@ -49,7 +49,7 @@ TEST(TEST_CATEGORY, atomic_operations_long) {
   const int start = 1;  // Avoid zero for division.
   const int end   = 11;
   for (int i = start; i < end; ++i) {
-    /*ASSERT_TRUE((TestAtomicOperations::AtomicOperationsTestIntegralType<
+    ASSERT_TRUE((TestAtomicOperations::AtomicOperationsTestIntegralType<
                  long int, TEST_EXECSPACE>(start, end - i, 1)));
     ASSERT_TRUE((TestAtomicOperations::AtomicOperationsTestIntegralType<
                  long int, TEST_EXECSPACE>(start, end - i, 2)));
@@ -68,7 +68,7 @@ TEST(TEST_CATEGORY, atomic_operations_long) {
     ASSERT_TRUE((TestAtomicOperations::AtomicOperationsTestIntegralType<
                  long int, TEST_EXECSPACE>(start, end - i, 9)));
     ASSERT_TRUE((TestAtomicOperations::AtomicOperationsTestIntegralType<
-                 long int, TEST_EXECSPACE>(start, end - i, 11)));*/
+                 long int, TEST_EXECSPACE>(start, end - i, 11)));
     ASSERT_TRUE((TestAtomicOperations::AtomicOperationsTestIntegralType<
                  long int, TEST_EXECSPACE>(start, end - i, 12)));
   }
