5299addf23c68443c0c3c07b2afa73b113ed4493
Daniel Arndt <arndtd@ornl.gov>
2020-11-11 18:05:20 -0500

    SYCL parallel_for MDRangePolicy

--- core/src/SYCL/Kokkos_SYCL_Parallel_Range.hpp
+++ core/src/SYCL/Kokkos_SYCL_Parallel_Range.hpp
@@ -45,6 +45,8 @@
 #ifndef KOKKOS_SYCL_PARALLEL_RANGE_HPP_
 #define KOKKOS_SYCL_PARALLEL_RANGE_HPP_
 
+#include <impl/KokkosExp_IterateTileGPU.hpp>
+
 template <class FunctorType, class ExecPolicy>
 class Kokkos::Impl::ParallelFor<FunctorType, ExecPolicy,
                                 Kokkos::Experimental::SYCL> {
@@ -122,6 +124,175 @@ class Kokkos::Impl::ParallelFor<FunctorType, ExecPolicy,
     // it in USM shared memory.
     if constexpr (std::is_trivially_copyable_v<decltype(m_functor)>)
       sycl_direct_launch(m_policy, m_functor);
+    else
+      sycl_indirect_launch();
+  }
+
+  ParallelFor(const FunctorType& arg_functor, const Policy& arg_policy)
+      : m_functor(arg_functor), m_policy(arg_policy) {}
+};
+
+// ParallelFor
+template <class FunctorType, class... Traits>
+class Kokkos::Impl::ParallelFor<FunctorType, Kokkos::MDRangePolicy<Traits...>,
+                                Kokkos::Experimental::SYCL> {
+ public:
+  using Policy = Kokkos::MDRangePolicy<Traits...>;
+
+ private:
+  using array_index_type = typename Policy::array_index_type;
+  using index_type       = typename Policy::index_type;
+  using LaunchBounds     = typename Policy::launch_bounds;
+  using WorkTag          = typename Policy::work_tag;
+
+  const FunctorType m_functor;
+  const Policy m_policy;
+
+  ParallelFor()        = delete;
+  ParallelFor& operator=(ParallelFor const&) = delete;
+
+  cl::sycl::nd_range<3> compute_ranges() const {
+    if constexpr (Policy::rank == 2) {
+      cl::sycl::range<3> local_sizes(m_policy.m_tile[0], m_policy.m_tile[1], 1);
+      cl::sycl::range<3> global_sizes(m_policy.m_upper[0] - m_policy.m_lower[0],
+                                      m_policy.m_upper[1] - m_policy.m_lower[1],
+                                      1);
+      return {global_sizes, local_sizes};
+    }
+    if constexpr (Policy::rank == 3) {
+      cl::sycl::range<3> local_sizes(m_policy.m_tile[0], m_policy.m_tile[1],
+                                     m_policy.m_tile[2]);
+      cl::sycl::range<3> global_sizes(
+          m_policy.m_upper[0] - m_policy.m_lower[0],
+          m_policy.m_upper[1] - m_policy.m_lower[1],
+          m_policy.m_upper[2] - m_policy.m_lower[2]);
+      return {global_sizes, local_sizes};
+    }
+    if constexpr (Policy::rank == 4) {
+      // id0,id1 encoded within first index; id2 to second index; id3 to third
+      // index
+      cl::sycl::range<3> local_sizes(m_policy.m_tile[0] * m_policy.m_tile[1],
+                                     m_policy.m_tile[2], m_policy.m_tile[3]);
+      cl::sycl::range<3> global_sizes(
+          (m_policy.m_upper[0] - m_policy.m_lower[0]) *
+              (m_policy.m_upper[1] - m_policy.m_lower[1]),
+          m_policy.m_upper[2] - m_policy.m_lower[2],
+          m_policy.m_upper[3] - m_policy.m_lower[3]);
+      return {global_sizes, local_sizes};
+    }
+    if constexpr (Policy::rank == 5) {
+      // id0,id1 encoded within first index; id2,id3 to second index; id4 to
+      // third index
+      cl::sycl::range<3> local_sizes(m_policy.m_tile[0] * m_policy.m_tile[1],
+                                     m_policy.m_tile[2] * m_policy.m_tile[3],
+                                     m_policy.m_tile[4]);
+      cl::sycl::range<3> global_sizes(
+          (m_policy.m_upper[0] - m_policy.m_lower[0]) *
+              (m_policy.m_upper[1] - m_policy.m_lower[1]),
+          (m_policy.m_upper[2] - m_policy.m_lower[2]) *
+              (m_policy.m_upper[3] - m_policy.m_lower[3]),
+          m_policy.m_upper[4] - m_policy.m_lower[4]);
+      return {global_sizes, local_sizes};
+    }
+    if constexpr (Policy::rank == 6) {
+      // id0,id1 encoded within first index; id2,id3 to second index; id4,id5 to
+      // third index
+      cl::sycl::range<3> local_sizes(m_policy.m_tile[0] * m_policy.m_tile[1],
+                                     m_policy.m_tile[2] * m_policy.m_tile[3],
+                                     m_policy.m_tile[4] * m_policy.m_tile[5]);
+      cl::sycl::range<3> global_sizes(
+          (m_policy.m_upper[0] - m_policy.m_lower[0]) *
+              (m_policy.m_upper[1] - m_policy.m_lower[1]),
+          (m_policy.m_upper[2] - m_policy.m_lower[2]) *
+              (m_policy.m_upper[3] - m_policy.m_lower[3]),
+          (m_policy.m_upper[4] - m_policy.m_lower[4]) *
+              (m_policy.m_upper[5] - m_policy.m_lower[5]));
+      return {global_sizes, local_sizes};
+    }
+    static_assert(Policy::rank > 1 && Policy::rank < 7,
+                  "Kokkos::MDRange Error: Exceeded rank bounds with SYCL\n");
+  }
+
+  template <typename Functor>
+  void sycl_direct_launch(const Functor& functor) const {
+    // Convenience references
+    const Kokkos::Experimental::SYCL& space = m_policy.space();
+    Kokkos::Experimental::Impl::SYCLInternal& instance =
+        *space.impl_internal_space_instance();
+    cl::sycl::queue& q = *instance.m_queue;
+
+    q.wait_and_throw();
+
+    if (m_policy.m_num_tiles == 0) return;
+
+    auto& policy = m_policy;
+
+    q.submit([functor, this, policy](cl::sycl::handler& cgh) {
+      const auto range = compute_ranges();
+
+      cgh.parallel_for(range, [functor, policy](cl::sycl::nd_item<3> item) {
+        const auto local_x =
+            static_cast<typename Policy::index_type>(item.get_local_id(0));
+        const auto local_y =
+            static_cast<typename Policy::index_type>(item.get_local_id(1));
+        const auto local_z =
+            static_cast<typename Policy::index_type>(item.get_local_id(2));
+        const auto global_x =
+            static_cast<typename Policy::index_type>(item.get_group(0));
+        const auto global_y =
+            static_cast<typename Policy::index_type>(item.get_group(1));
+        const auto global_z =
+            static_cast<typename Policy::index_type>(item.get_group(2));
+        const auto n_global_x =
+            static_cast<typename Policy::index_type>(item.get_group_range(0));
+        const auto n_global_y =
+            static_cast<typename Policy::index_type>(item.get_group_range(1));
+        const auto n_global_z =
+            static_cast<typename Policy::index_type>(item.get_group_range(2));
+
+        Kokkos::Impl::DeviceIterateTile<Policy::rank, Policy, Functor,
+                                        typename Policy::work_tag>(
+            policy, functor, {n_global_x, n_global_y, n_global_z},
+            {global_x, global_y, global_z}, {local_x, local_y, local_z})
+            .exec_range();
+      });
+    });
+
+    q.wait_and_throw();
+  }
+
+  // Indirectly launch a functor by explicitly creating it in USM shared memory
+  void sycl_indirect_launch() const {
+    // Convenience references
+    const Kokkos::Experimental::SYCL& space = m_policy.space();
+    Kokkos::Experimental::Impl::SYCLInternal& instance =
+        *space.impl_internal_space_instance();
+    Kokkos::Experimental::Impl::SYCLInternal::IndirectKernelMemory& kernelMem =
+        *instance.m_indirectKernel;
+
+    // Allocate USM shared memory for the functor
+    kernelMem.resize(std::max(kernelMem.size(), sizeof(m_functor)));
+
+    // Placement new a copy of functor into USM shared memory
+    //
+    // Store it in a unique_ptr to call its destructor on scope exit
+    std::unique_ptr<FunctorType, Kokkos::Impl::destruct_delete>
+        kernelFunctorPtr(new (kernelMem.data()) FunctorType(m_functor));
+
+    // Use reference_wrapper (because it is both trivially copyable and
+    // invocable) and launch it
+    sycl_direct_launch(std::reference_wrapper(*kernelFunctorPtr));
+  }
+
+ public:
+  using functor_type = FunctorType;
+
+  void execute() const {
+    // if the functor is trivially copyable, we can launch it directly;
+    // otherwise, we will launch it indirectly via explicitly creating
+    // it in USM shared memory.
+    if constexpr (std::is_trivially_copyable_v<decltype(m_functor)>)
+      sycl_direct_launch(m_functor);
     else
       sycl_indirect_launch();
   }
