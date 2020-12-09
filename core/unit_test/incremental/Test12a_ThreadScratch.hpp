2dd7c424e76f6f86dc2af70ca5eeb1fdda8c2b32
Daniel Arndt <arndtd@ornl.gov>
2020-12-09 22:38:09 +0000

    Intermediate

--- core/unit_test/incremental/Test12a_ThreadScratch.hpp
+++ core/unit_test/incremental/Test12a_ThreadScratch.hpp
@@ -68,21 +68,21 @@ struct ThreadScratch {
 	  (void) team;
     // Allocate and use scratch pad memory
    scratch_t v_S(team.thread_scratch(1), sY);
-   // int n = team.league_rank();
+   int n = team.league_rank();
 
- //   for (int i = 0; i < sY; ++i) v_S(i) = 0;
+   for (int i = 0; i < sY; ++i) v_S(i) = 0;
 
-/*    Kokkos::parallel_for(Kokkos::TeamThreadRange(team, sX), [&](const int m) {
+   Kokkos::parallel_for(Kokkos::TeamThreadRange(team, sX), [&](const int m) {
       Kokkos::parallel_for(
           Kokkos::ThreadVectorRange(team, sY),
           [&](const int k) { v_S(k) += sX * sY * n + sY * m + k; });
-    });*/
+    });
 
-    //team.team_barrier();
+    team.team_barrier();
 
-//    for (int i = 0; i < sY; ++i) {
-  //    v(n, team.team_rank()) += v_S(i);
-    //}
+    for (int i = 0; i < sY; ++i) {
+      v(n, team.team_rank()) += v_S(i);
+    }
   }
 
   void run(const int pN, const int sX_, const int sY_) {
@@ -100,11 +100,11 @@ struct ThreadScratch {
     Kokkos::parallel_for(
         "Test12a_ThreadScratch",
         policy_t(pN, max_team_size)
-            //.set_scratch_size(1, Kokkos::PerThread(scratchSize))
+            .set_scratch_size(1, Kokkos::PerThread(scratchSize))
 	    ,
         *this);
 
-/*    Kokkos::fence();
+    Kokkos::fence();
     auto v_H = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), v);
 
     size_t check   = 0;
@@ -113,15 +113,15 @@ struct ThreadScratch {
       for (int m = 0; m < max_team_size; ++m) {
         check += v_H(n, m);
       }
-    ASSERT_EQ(s * (s - 1) / 2, check);*/
+    ASSERT_EQ(s * (s - 1) / 2, check);
   }
 };
 
 TEST(TEST_CATEGORY, IncrTest_12a_ThreadScratch) {
   ThreadScratch<TEST_EXECSPACE> test;
   test.run(1, 55, 9);
-//  test.run(2, 4, 22);
-//  test.run(14, 277, 321);
+  //test.run(2, 4, 22);
+  //test.run(14, 277, 321);
 }
 
 }  // namespace Test
