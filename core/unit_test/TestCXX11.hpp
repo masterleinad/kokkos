21d9b1362c359d109aa2334fceeeb4e808f46ae9
Daniel Arndt <arndtd@ornl.gov>
2021-01-15 17:57:05 +0000

    Address @dalg24's comments

--- core/unit_test/TestCXX11.hpp
+++ core/unit_test/TestCXX11.hpp
@@ -371,8 +371,6 @@ bool Test(int test) {
 }  // namespace TestCXX11
 
 namespace Test {
-// FIXME_SYCL requires TeamPolicy
-#ifndef KOKKOS_ENABLE_SYCL
 TEST(TEST_CATEGORY, cxx11) {
   if (std::is_same<Kokkos::DefaultExecutionSpace, TEST_EXECSPACE>::value) {
     ASSERT_TRUE((TestCXX11::Test<TEST_EXECSPACE>(1)));
@@ -381,6 +379,5 @@ TEST(TEST_CATEGORY, cxx11) {
     ASSERT_TRUE((TestCXX11::Test<TEST_EXECSPACE>(4)));
   }
 }
-#endif
 
 }  // namespace Test
