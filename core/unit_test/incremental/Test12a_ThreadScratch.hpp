ec65114d9e8c0bebcdda1992edcc8d9ff024b2c2
Daniel Arndt <arndtd@ornl.gov>
2021-02-03 09:46:07 -0500

    Improve calculation of maximal team size

--- core/unit_test/incremental/Test12a_ThreadScratch.hpp
+++ core/unit_test/incremental/Test12a_ThreadScratch.hpp
@@ -122,10 +122,7 @@ TEST(TEST_CATEGORY, IncrTest_12a_ThreadScratch) {
   ThreadScratch<TEST_EXECSPACE> test;
   test.run(1, 55, 9);
   test.run(2, 4, 22);
-  // FIXME_SYCL this gives a wrong result yet
-#ifndef KOKKOS_ENABLE_SYCL
   test.run(14, 277, 321);
-#endif
 }
 
 }  // namespace Test
