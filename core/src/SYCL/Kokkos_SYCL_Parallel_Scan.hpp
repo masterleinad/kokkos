9c6eb99da30f9509d100c9b169382b88302c52cb
Daniel Arndt <arndtd@ornl.gov>
2021-07-14 16:14:55 -0400

    Use sycl::group_barrier

--- core/src/SYCL/Kokkos_SYCL_Parallel_Scan.hpp
+++ core/src/SYCL/Kokkos_SYCL_Parallel_Scan.hpp
@@ -173,7 +173,7 @@ class ParallelScanSYCLBase {
                              &global_mem[global_id]);
             else
               ValueInit::init(functor, &local_mem[local_id]);
-            item.barrier(sycl::access::fence_space::local_space);
+            sycl::group_barrier(item.get_group());
 
             // Perform workgroup reduction
             for (size_t stride = 1; 2 * stride < wgroup_size + 1; stride *= 2) {
@@ -181,7 +181,7 @@ class ParallelScanSYCLBase {
               if (idx < wgroup_size)
                 ValueJoin::join(functor, &local_mem[idx],
                                 &local_mem[idx - stride]);
-              item.barrier(sycl::access::fence_space::local_space);
+              sycl::group_barrier(item.get_group());
             }
 
             if (local_id == 0) {
@@ -205,7 +205,7 @@ class ParallelScanSYCLBase {
                                &local_mem[idx]);
                 ValueJoin::join(functor, &local_mem[idx], &dummy);
               }
-              item.barrier(sycl::access::fence_space::local_space);
+              sycl::group_barrier(item.get_group());
             }
 
             // Write results to global memory
