961c139681aabc335674d9ec6bb3d0520f7b7347
Daniel Arndt <arndtd@ornl.gov>
2023-03-27 21:46:13 +0000

    Still failing

--- core/src/SYCL/Kokkos_SYCL_View.hpp
+++ core/src/SYCL/Kokkos_SYCL_View.hpp
@@ -28,6 +28,30 @@
 namespace Kokkos {
 namespace Impl {
 
+template <typename ValueType, typename MemorySpace>
+struct SYCLUSMHandle {
+  using usm_ptr_type =
+      std::conditional_t<is_sycl_type_space<MemorySpace>::value,
+                         sycl::global_ptr<ValueType>,
+                         sycl::device_ptr<ValueType>>;
+
+  usm_ptr_type m_ptr;
+
+  template <typename iType>
+  KOKKOS_FUNCTION ValueType& operator[](const iType& i) {
+    return m_ptr[i];
+  }
+
+  KOKKOS_FUNCTION
+  operator ValueType*() const { return m_ptr.get(); }
+
+  KOKKOS_DEFAULTED_FUNCTION
+  SYCLUSMHandle() = default;
+
+  KOKKOS_FUNCTION
+  explicit SYCLUSMHandle(ValueType* const arg_ptr) : m_ptr(arg_ptr) {}
+};
+
 template <class Traits>
 struct ViewDataHandle<
     Traits,
@@ -41,22 +65,19 @@ struct ViewDataHandle<
                         ScratchMemorySpace<Kokkos::Experimental::SYCL>>)>> {
   using value_type   = typename Traits::value_type;
   using memory_space = typename Traits::memory_space;
-  using handle_type =
-      std::conditional_t<is_sycl_type_space<memory_space>::value,
-                         sycl::global_ptr<value_type>,
-                         sycl::device_ptr<value_type>>;
+  using handle_type = SYCLUSMHandle<value_type,memory_space>;
   using return_type = typename Traits::value_type&;
   using track_type  = Kokkos::Impl::SharedAllocationTracker;
 
   KOKKOS_INLINE_FUNCTION
-  static value_type* assign(value_type* arg_data_ptr,
+  static handle_type assign(value_type* arg_data_ptr,
                             track_type const& /*arg_tracker*/) {
-    return (value_type*)(arg_data_ptr);
+    return arg_data_ptr;
   }
 
   KOKKOS_INLINE_FUNCTION
-  static value_type* assign(handle_type const arg_data_ptr, size_t offset) {
-    return (value_type*)(arg_data_ptr + offset);
+  static handle_type assign(handle_type const arg_data_ptr, size_t offset) {
+    return (arg_data_ptr + offset);
   }
 };
 
