83144690814c679c87e0ec233305921bccc4e210
Daniel Arndt <arndtd@ornl.gov>
2023-04-20 13:40:53 +0000

    Fix sign-comparison warnings

--- core/src/SYCL/Kokkos_SYCL_Parallel_Scan.hpp
+++ core/src/SYCL/Kokkos_SYCL_Parallel_Scan.hpp
@@ -156,9 +156,9 @@ class ParallelScanSYCLBase {
                 functor_reducer.get_reducer();
 
             const auto n_wgroups   = item.get_group_range()[0];
-            const auto wgroup_size = item.get_local_range()[0];
+            const int wgroup_size = item.get_local_range()[0];
 
-            const index_type local_id  = item.get_local_linear_id();
+            const int local_id  = item.get_local_linear_id();
             const index_type global_id = item.get_global_linear_id();
 
             // Initialize local memory
@@ -200,7 +200,7 @@ class ParallelScanSYCLBase {
                 else
                   reducer.init(&local_value);
                 workgroup_scan<>(item, reducer, local_mem, local_value,
-                                 std::min(n_wgroups - offset, wgroup_size));
+                                 std::min<index_type>(n_wgroups - offset, wgroup_size));
                 if (id < static_cast<index_type>(n_wgroups)) {
                   reducer.join(&local_value, &total);
                   group_results[id] = local_value;
