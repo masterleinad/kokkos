8f67e27cafdb27c71dc6bec12dc414a841e4ee1a
Daniel Arndt <arndtd@ornl.gov>
2023-05-03 17:54:40 +0000

    Fix SYCL

--- core/src/impl/Kokkos_Printf.hpp
+++ core/src/impl/Kokkos_Printf.hpp
@@ -31,7 +31,6 @@ namespace Kokkos {
 template <typename... Args>
 KOKKOS_FUNCTION int printf(const char* format, Args... args) {
 #ifdef __SYCL_DEVICE_ONLY__
-  //const __attribute__((opencl_constant)) char fmt[] = (format);
   return sycl::ext::oneapi::experimental::printf(format, args...);
 #else
   return std::printf(format, args...);
