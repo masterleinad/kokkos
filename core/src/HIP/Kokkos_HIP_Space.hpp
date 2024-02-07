2395f0ddaf21e4eca6e23024cf9087d6f9cecb6e
Daniel Arndt <arndtd@ornl.gov>
2024-02-06 21:35:56 -0500

    Fix Cuda

--- core/src/HIP/Kokkos_HIP_Space.hpp
+++ core/src/HIP/Kokkos_HIP_Space.hpp
@@ -134,12 +134,12 @@ class HIPHostPinnedSpace {
   ~HIPHostPinnedSpace()                                        = default;
 
   /**\brief  Allocate untracked memory in the space */
-  // FIXME_HIP Use execution space instance
-  void* allocate(const HIP&, const size_t arg_alloc_size) const {
+  template <typename ExecutionSpace>
+  void* allocate(const ExecutionSpace&, const size_t arg_alloc_size) const {
     return allocate(arg_alloc_size);
   }
-  // FIXME_HIP Use execution space instance
-  void* allocate(const HIP&, const char* arg_label, const size_t arg_alloc_size,
+  template <typename ExecutionSpace>
+  void* allocate(const ExecutionSpace&, const char* arg_label, const size_t arg_alloc_size,
                  const size_t arg_logical_size = 0) const {
     return allocate(arg_label, arg_alloc_size, arg_logical_size);
   }
@@ -206,12 +206,12 @@ class HIPManagedSpace {
   ~HIPManagedSpace()                                     = default;
 
   /**\brief  Allocate untracked memory in the space */
-  // FIXME_HIP Use execution space instance
-  void* allocate(const HIP&, const size_t arg_alloc_size) const {
+   template <typename ExecutionSpace>
+  void* allocate(const ExecutionSpace&, const size_t arg_alloc_size) const {
     return allocate(arg_alloc_size);
   }
-  // FIXME_HIP Use execution space instance
-  void* allocate(const HIP&, const char* arg_label, const size_t arg_alloc_size,
+  template <typename ExecutionSpace>
+  void* allocate(const ExecutionSpace&, const char* arg_label, const size_t arg_alloc_size,
                  const size_t arg_logical_size = 0) const {
     return allocate(arg_label, arg_alloc_size, arg_logical_size);
   }
