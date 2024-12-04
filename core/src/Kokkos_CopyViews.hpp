31b2737809a70617c05801b95a3377691c4bb901
Daniel Arndt <arndtd@ornl.gov>
2024-12-04 10:00:01 -0500

    Indentation
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- core/src/Kokkos_CopyViews.hpp
+++ core/src/Kokkos_CopyViews.hpp
@@ -861,7 +861,7 @@ struct CommonSubview {
 };
 
 template <bool SequentialHostInit, class DstType, class SrcType,
-          int Rank        = DstType::rank>
+          int Rank = DstType::rank>
 struct ViewRemap;
 
 template <bool SequentialHostInit, class DstType, class SrcType>
@@ -927,8 +927,7 @@ auto create_common_subview_no_match(const DstType& dst, const SrcType& src,
   return common_subview;
 }
 
-template <bool SequentialHostInit, class DstType, class SrcType,
-          int Rank>
+template <bool SequentialHostInit, class DstType, class SrcType, int Rank>
 struct ViewRemap {
   using p_type = Kokkos::pair<int64_t, int64_t>;
 
