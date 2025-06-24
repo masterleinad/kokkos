b00c589aff99bdc83e9a12ecf543d0eb240d4fbb
Daniel Arndt <arndtd@ornl.gov>
2025-06-24 16:58:25 -0400

    Fix jenkins CI
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- .jenkins/continuous.groovy
+++ .jenkins/continuous.groovy
@@ -56,7 +56,7 @@ pipeline {
                                 -DKokkos_ENABLE_EXAMPLES=ON \
                                 -DKokkos_ENABLE_SERIAL=ON && \
                               cmake --build build --target install -j 8 && \
-                              ctest --test-dir build --no-compress-output -T Test --verbose && \
+                              ctest --test-dir build --no-compress-output -T Test --verbose'''
                     }
                     post {
                         always {
