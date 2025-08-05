417fa975c094b4a4aa926edb7029344a2af08f4b
Daniel Arndt <arndtd@ornl.gov>
2025-08-05 19:03:37 -0400

    FIX

--- core/unit_test/TestReduce.hpp
+++ core/unit_test/TestReduce.hpp
@@ -720,7 +720,7 @@ TEST(TEST_CATEGORY, reduction_identity_min_max_floating_point_types) {
   // nvhpc on device doesn't use the correct neutral value for the min and max
   // reducers
 #if !(defined(KOKKOS_COMPILER_NVHPC) && defined(KOKKOS_ENABLE_OPENACC))
-#if !(defined(__FINITE_MATH_ONLY__) && __FINITE_MATH_ONLY__ > 0)
+#if defined(__FINITE_MATH_ONLY__) && __FINITE_MATH_ONLY__ > 0
   GTEST_SKIP() << "skipping when assuming finite math";
 #endif
   TestReductionOverInfiniteFloat<Kokkos::Experimental::half_t>();
