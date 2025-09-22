3bf881af6f2c02dbec5167f656fad94c3c28ba12
Daniel Arndt <arndtd@ornl.gov>
2025-09-22 10:57:27 -0400

    Comment problematic check instead
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- core/unit_test/TestTimer.hpp
+++ core/unit_test/TestTimer.hpp
@@ -28,21 +28,24 @@ TEST(TEST_CATEGORY, timer) {
   using namespace std::chrono_literals;
 
   Kokkos::Timer t;
-  std::this_thread::sleep_for(50ms);
+  std::this_thread::sleep_for(5ms);
   auto elapsed = t.seconds();
-  EXPECT_GE(elapsed, .05);
+  EXPECT_GE(elapsed, .005);
   EXPECT_LT(elapsed, 1.);
 
-  std::this_thread::sleep_for(50ms);
+  std::this_thread::sleep_for(10ms);
   auto elapsed2 = std::as_const(t).seconds();
-  EXPECT_GE(elapsed2, .1);
+  EXPECT_GE(elapsed2, .015);
   EXPECT_GT(elapsed2, elapsed);
 
   t.reset();
-  std::this_thread::sleep_for(50ms);
+  std::this_thread::sleep_for(5ms);
   auto elapsed3 = t.seconds();
-  EXPECT_GE(elapsed3, .05);
-  EXPECT_LT(elapsed3, elapsed2);
+  EXPECT_GE(elapsed3, .005);
+  // Using the line below turned out to be problematic since there is no
+  // guaranteed upper bound for the time std::this_thread::sleep_for is
+  // blocking for.
+  // EXPECT_LT(elapsed3, elapsed2);
 }
 
 static_assert(!std::is_copy_constructible_v<Kokkos::Timer>);
