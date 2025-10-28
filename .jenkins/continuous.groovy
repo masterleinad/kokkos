8a3ee39243cb9acabb175a0d00dd4d730ca0770f
Daniel Arndt <arndtd@ornl.gov>
2025-10-28 10:18:56 -0400

    Fix CUDA-12.2-NVCC-RDC
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- .jenkins/continuous.groovy
+++ .jenkins/continuous.groovy
@@ -222,7 +222,6 @@ pipeline {
                               export CMAKE_PREFIX_PATH=${PWD}/../install && \
                               cmake \
                                 -DCMAKE_BUILD_TYPE=Release \
-                                -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
                                 -DCMAKE_CXX_COMPILER=$WORKSPACE/bin/nvcc_wrapper \
                                 -DCMAKE_CXX_FLAGS="-Werror --Werror=all-warnings -Xcudafe --diag_suppress=940" \
                                 -DCMAKE_EXE_LINKER_FLAGS="-Xnvlink -suppress-stack-size-warning" \
