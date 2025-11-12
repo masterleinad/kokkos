3bbfbc2c36c8d4c56dadd784a6b36fe83526f258
Daniel Arndt <arndtd@ornl.gov>
2025-11-12 07:44:15 -0500

    Remove from builds missing the path
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- .jenkins/continuous.groovy
+++ .jenkins/continuous.groovy
@@ -346,7 +346,6 @@ pipeline {
                                 -DKokkos_ENABLE_COMPILER_WARNINGS=ON \
                                 -DKokkos_ENABLE_DEPRECATED_CODE_4=OFF \
                                 -DKokkos_ENABLE_TESTS=ON \
-                                -DCUDA_cuda_driver_LIBRARY=/usr/lib64/libcuda.so \
                                 -DKokkos_ENABLE_CUDA=ON \
                                 -DKokkos_ENABLE_OPENMP=ON \
                               .. && \
@@ -522,7 +521,6 @@ pipeline {
                                 -DKokkos_ENABLE_DEPRECATED_CODE_4=ON \
                                 -DKokkos_ENABLE_TESTS=ON \
                                 -DKokkos_ENABLE_BENCHMARKS=ON \
-                                -DCUDA_cuda_driver_LIBRARY=/usr/lib64/libcuda.so \
                                 -DKokkos_ENABLE_CUDA=ON \
                                 -DKokkos_ENABLE_TUNING=ON \
                                 -DKokkos_ARCH_VOLTA70=ON \
@@ -564,7 +562,6 @@ pipeline {
                                 -DKokkos_ENABLE_DEPRECATED_CODE_4=ON \
                                 -DKokkos_ENABLE_TESTS=ON \
                                 -DKokkos_ENABLE_BENCHMARKS=ON \
-                                -DCUDA_cuda_driver_LIBRARY=/usr/lib64/libcuda.so \
                                 -DKokkos_ENABLE_CUDA=ON \
                                 -DKokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE=ON \
                                 -DKokkos_ENABLE_TUNING=ON \
