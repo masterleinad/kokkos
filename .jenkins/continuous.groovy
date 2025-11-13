eaff95e4606daeef252764c22f981f58bfa85be2
Daniel Arndt <arndtd@ornl.gov>
2025-11-13 10:19:58 -0500

    Enable CUDA-12.2-NVCC-RDC build again
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- .jenkins/continuous.groovy
+++ .jenkins/continuous.groovy
@@ -183,7 +183,7 @@ pipeline {
                         }
                     }
                 }
-/*
+
                 stage('CUDA-12.2-NVCC-RDC') {
                     agent {
                         dockerfile {
@@ -274,10 +274,11 @@ pipeline {
                     post {
                         always {
                             sh 'ccache --show-stats'
+                            xunit([CTest(deleteOutputFiles: true, failIfNotNew: true, pattern: 'build-tests/Testing/**/Test.xml', skipNoTestFiles: false, stopProcessingIfError: true)])
                         }
                     }
                 }
-*/
+
             }
         }
         stage('Build-2') {
