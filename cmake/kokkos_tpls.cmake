5cf26162c8ec7746ae0e76de03e871e080658561
Jakob Bludau <104908666+JBludau@users.noreply.github.com>
2025-12-17 08:36:15 -0500

    Rework our cmake to detect ROCm TPL (#8747)
    * modernize rocm tpl module
    * use cmake_prefix_path for rocm location (as rocm searches for other libs internally, it fails to find them with ROCM_PATH for rocm 6.2 ... newer versions do not have this problem
    
    ---------
    
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- cmake/kokkos_tpls.cmake
+++ cmake/kokkos_tpls.cmake
@@ -21,31 +21,8 @@ endfunction()
 
 kokkos_tpl_option(HWLOC Off TRIBITS HWLOC)
 kokkos_tpl_option(CUDA ${Kokkos_ENABLE_CUDA} TRIBITS CUDA)
-if(KOKKOS_ENABLE_HIP AND NOT KOKKOS_CXX_COMPILER_ID STREQUAL HIPCC)
-  set(ROCM_DEFAULT ON)
-else()
-  set(ROCM_DEFAULT OFF)
-endif()
-if(KOKKOS_ENABLE_HIP)
-  set(ROCTHRUST_DEFAULT ON)
-else()
-  set(ROCTHRUST_DEFAULT OFF)
-endif()
-kokkos_tpl_option(ROCM ${ROCM_DEFAULT})
-kokkos_tpl_option(ROCTHRUST ${ROCTHRUST_DEFAULT})
-if(Kokkos_ENABLE_ROCTHRUST)
-  include(CheckCXXSourceCompiles)
-  check_cxx_source_compiles(
-    "
-    #include <ios>
-    int main() {
-      static_assert(_GLIBCXX_RELEASE < 9);
-      return 0;
-    }
-    "
-    Kokkos_ENABLE_IMPL_SKIP_NO_RTTI_FLAG
-  )
-endif()
+kokkos_tpl_option(ROCM ${Kokkos_ENABLE_HIP})
+kokkos_tpl_option(ROCTHRUST ${Kokkos_ENABLE_HIP})
 
 if(KOKKOS_ENABLE_SYCL)
   set(ONEDPL_DEFAULT ON)
