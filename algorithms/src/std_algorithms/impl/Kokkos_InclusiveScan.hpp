de2af7191fac1efe818af0f0fda94a1183b9b3ee
Daniel Arndt <arndtd@ornl.gov>
2025-12-29 08:50:49 -0500

    Leave HIP code alone
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- algorithms/src/std_algorithms/impl/Kokkos_InclusiveScan.hpp
+++ algorithms/src/std_algorithms/impl/Kokkos_InclusiveScan.hpp
@@ -185,11 +185,7 @@ OutputIteratorType inclusive_scan_default_op_exespace_impl(
 
   Kokkos::Profiling::popRegion();
 
-#if CUDA_VERSION >= 13010
-  const auto num_elements = cuda::std::distance(first_from, last_from);
-#else
   const auto num_elements = thrust::distance(first_from, last_from);
-#endif
 
   return first_dest + num_elements;
 }
@@ -280,11 +276,8 @@ OutputIteratorType inclusive_scan_custom_binary_op_exespace_impl(
                          binary_op);
 
   Kokkos::Profiling::popRegion();
-#if CUDA_VERSION >= 13010
-  const auto num_elements = cuda::std::distance(first_from, last_from);
-#else
+
   const auto num_elements = thrust::distance(first_from, last_from);
-#endif
 
   return first_dest + num_elements;
 }
