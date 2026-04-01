e6fc6df5d1991c8b5a7dc57c282c6c49b856fc93
Daniel Arndt <arndtd@ornl.gov>
2026-04-01 17:11:15 -0400

    Unconditionally disable hipMallocAsync by default
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- cmake/kokkos_enable_options.cmake
+++ cmake/kokkos_enable_options.cmake
@@ -89,18 +89,8 @@ kokkos_enable_option(
   HIP_MULTIPLE_KERNEL_INSTANTIATIONS OFF
   "Whether multiple kernels are instantiated at compile time - improve performance but increase compile time"
 )
-# FIXME_HIP
-if(KOKKOS_ENABLE_HIP)
-  #here just for the version, can be removed with the fixme as it will be found by our TPL processing
-  find_package(hip REQUIRED PATHS ${ROCM_PATH} $ENV{ROCM_PATH})
-endif()
-if(hip_VERSION VERSION_GREATER_EQUAL 7.0.0)
-  set(HIP_MALLOC_ASYNC_DEFAULT OFF)
-else()
-  set(HIP_MALLOC_ASYNC_DEFAULT ${KOKKOS_ENABLE_HIP})
-endif()
-kokkos_enable_option(IMPL_HIP_MALLOC_ASYNC ${HIP_MALLOC_ASYNC_DEFAULT} "Whether to enable hipMallocAsync")
-if((hip_VERSION VERSION_GREATER_EQUAL 7.0.0) AND Kokkos_ENABLE_IMPL_HIP_MALLOC_ASYNC)
+kokkos_enable_option(IMPL_HIP_MALLOC_ASYNC OFF "Whether to enable hipMallocAsync")
+if(Kokkos_ENABLE_IMPL_HIP_MALLOC_ASYNC)
   message(WARNING "Using Kokkos_ENABLE_IMPL_HIP_MALLOC_ASYNC is problematic with ROCm 7")
 endif()
 
