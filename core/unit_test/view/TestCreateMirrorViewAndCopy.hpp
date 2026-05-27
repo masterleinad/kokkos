a64c934a4d82c8382519dd757682ce76ca110fec
Daniel Arndt <arndtd@ornl.gov>
2026-05-27 10:07:23 -0400

    Test full type
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- core/unit_test/view/TestCreateMirrorViewAndCopy.hpp
+++ core/unit_test/view/TestCreateMirrorViewAndCopy.hpp
@@ -14,20 +14,22 @@ namespace {
 
 template <class MemorySpace>
 void test_create_mirror_view_and_copy() {
-  const int N = 10;
-  Kokkos::View<int*, MemorySpace> device_view("device_view", N);
-  Kokkos::deep_copy(device_view, 1);
-  auto device_view_copy   = Kokkos::create_mirror_view_and_copy(device_view);
-  using copy_memory_space = decltype(device_view_copy)::memory_space;
-  static_assert(
-      std::is_same_v<
-          copy_memory_space,
-          typename decltype(device_view)::host_mirror_type::memory_space>);
+  const int N              = 10;
+  using original_view_type = Kokkos::View<int*, MemorySpace>;
+  original_view_type original_view("original_view", N);
+  Kokkos::deep_copy(original_view, 1);
+  auto original_view_copy = Kokkos::create_mirror_view_and_copy(original_view);
+  using copy_memory_space = decltype(original_view_copy)::memory_space;
+  static_assert(std::is_same_v<
+                decltype(original_view_copy),
+                typename Kokkos::Impl::MirrorViewType<
+                    typename original_view_type::host_mirror_type::memory_space,
+                    int*, MemorySpace>::view_type>);
   if (std::is_same_v<copy_memory_space, MemorySpace>)
-    ASSERT_EQ(device_view_copy, device_view);
+    ASSERT_EQ(original_view_copy, original_view);
   else
-    ASSERT_NE(device_view_copy, device_view);
-  for (int i = 0; i < N; ++i) ASSERT_EQ(device_view_copy(i), 1);
+    ASSERT_NE(original_view_copy, original_view);
+  for (int i = 0; i < N; ++i) ASSERT_EQ(original_view_copy(i), 1);
 }
 
 TEST(TEST_CATEGORY, view_create_mirror_view_and_copy) {
