3786cac62defcdd55c7aea64263e706533f95886
Daniel Arndt <arndtd@ornl.gov>
2026-07-23 09:18:03 -0400

    KOKKOS_SYCL_KERNEL_SIZE_LIMIT -> KOKKOS_IMPL_SYCL_KERNEL_SIZE_LIMIT
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- core/src/SYCL/Kokkos_SYCL_Instance.hpp
+++ core/src/SYCL/Kokkos_SYCL_Instance.hpp
@@ -209,21 +209,21 @@ class SYCLInternal {
 // GPUS for all arguments handed to a kernel, assume for now that the rest
 // doesn't need more than roughly 256 bytes.
 #ifdef KOKKOS_IMPL_ARCH_NVIDIA_GPU
-#define KOKKOS_SYCL_KERNEL_SIZE_LIMIT 3750
+#define KOKKOS_IMPL_SYCL_KERNEL_SIZE_LIMIT 3750
 #else  // assume the stricter Intel GPU limit
-#define KOKKOS_SYCL_KERNEL_SIZE_LIMIT 1800
+#define KOKKOS_IMPL_SYCL_KERNEL_SIZE_LIMIT 1800
 #endif
 #if defined(SYCL_DEVICE_COPYABLE)
 template <typename Functor, typename Storage,
-          bool ManualCopy = (sizeof(Functor) >= KOKKOS_SYCL_KERNEL_SIZE_LIMIT)>
+          bool ManualCopy = (sizeof(Functor) >= KOKKOS_IMPL_SYCL_KERNEL_SIZE_LIMIT)>
 class SYCLFunctionWrapper;
 #else
 template <typename Functor, typename Storage,
-          bool ManualCopy = (sizeof(Functor) >= KOKKOS_SYCL_KERNEL_SIZE_LIMIT ||
+          bool ManualCopy = (sizeof(Functor) >= KOKKOS_IMPL_SYCL_KERNEL_SIZE_LIMIT ||
                              !std::is_trivially_copyable_v<Functor>)>
 class SYCLFunctionWrapper;
 #endif
-#undef KOKKOS_SYCL_KERNEL_SIZE_LIMIT
+#undef KOKKOS_IMPL_SYCL_KERNEL_SIZE_LIMIT
 
 #if defined(SYCL_DEVICE_COPYABLE)
 template <typename Functor, typename Storage>
