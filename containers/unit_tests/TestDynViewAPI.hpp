1be0d1e6a51b477e81c267ad9e08b2ac54ab3f92
Daniel Arndt <arndtd@ornl.gov>
2026-08-19 19:42:02 +0000

    Disable for another test
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- containers/unit_tests/TestDynViewAPI.hpp
+++ containers/unit_tests/TestDynViewAPI.hpp
@@ -4,7 +4,9 @@
 #include <gtest/gtest.h>
 
 #include <Kokkos_Macros.hpp>
-#ifdef KOKKOS_ENABLE_EXPERIMENTAL_CXX20_MODULES
+// FIXME_MODULES The Impl::ApplyToViewOfStaticRank specialization for
+// DynRankView isn't visible
+#if 0  // KOKKOS_ENABLE_EXPERIMENTAL_CXX20_MODULES
 import kokkos.core;
 import kokkos.dyn_rank_view;
 import kokkos.dyn_rank_view_impl;
