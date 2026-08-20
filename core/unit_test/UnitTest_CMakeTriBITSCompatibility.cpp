bb41d10411d4b7abd0dbdd6332aafd27c2f41c13
Daniel Arndt <arndtd@ornl.gov>
2026-08-20 15:30:01 -0400

    Revert changes to UnitTest_CMakeTriBITSCompatibility
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- core/unit_test/UnitTest_CMakeTriBITSCompatibility.cpp
+++ core/unit_test/UnitTest_CMakeTriBITSCompatibility.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
   if (std::getenv("KOKKOS_TEST_TRIBITS_COMPATIBILITY")) {
     return EXIT_SUCCESS;
   }
-  if (argc >= 2 && std::string_view(argv[1]).find(
+  if (argc == 2 && std::string_view(argv[1]).find(
                        "--kokkos-test-tribits-compatibility") == 0) {
     return EXIT_SUCCESS;
   }
